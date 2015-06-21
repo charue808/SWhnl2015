a139d0ce06b6961e5a4203d403601eaa4e4cf64b
mdctenga <mdctenga@gmail.com>
2015-06-21 03:01:05 -1000

    Changed logo and tweaked amount styles so that it is more balanced on the page.

--- code/server/fixtures.js
+++ code/server/fixtures.js
@@ -5,23 +5,23 @@ if (Books.find().count() === 0) {
     author: "Hemingway, Ernest",
     cost: "$10"
   });
-    
+
   Books.insert({
-      cover: "https://upload.wikimedia.org/wikipedia/en/c/c4/TheAlchemist.jpg",
+      cover: "http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0062315005&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wwwreadersgro-20",
       title: "The Alchemist",
       author: "Coelho, Paul",
       cost: "$5"
   });
-    
+
     Books.insert({
         cover: "http://www.nostarch.com/sites/default/files/imagecache/product_main_page/manga_guide_to_physics.png",
         title: "Manga Guide to Physics",
         author: "Nitta, Hideo",
         cost: "$5"
     });
-    
+
     Books.insert({
-        cover: "https://books.google.com/books/content?id=U9UgyWiCcrAC&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE70uijTZeNjeDKXj8itJAWtxYAMOKT-r9cjsy8G414Z1vCsGypH9FYgDded5s14SGlXxd1HyaupmuJQroB-gp89tsvvkuhz5D4Ro9fCObWNkLvIiypJ4Di5xkiuhsEQRJSfoqOiw",
+        cover: "http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00768DB2S&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wwwreadersgro-20",
         title: "The Wise Men",
         author: "Issacson, Walter",
         cost: "$15"
